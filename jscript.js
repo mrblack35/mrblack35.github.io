e32af0cf07e3cfa18e875a80a9da9236b2915f64
mrblack35 <32855460+mrblack35@users.noreply.github.com>
2020-06-06 19:15:56 +0100

    Initial Commit

---
+++ jscript.js
@@ -0,0 +1,23 @@
+function displayElement1() {
+    document.getElementById("pbox1").style.display = "block";
+
+    document.getElementById("pbox2").style.display = "none";
+
+    document.getElementById("pbox3").style.display = "none";
+}
+
+function displayElement2() {
+    document.getElementById("pbox2").style.display = "block";
+
+    document.getElementById("pbox1").style.display = "none";
+
+    document.getElementById("pbox3").style.display = "none";
+}
+
+function displayElement3() {
+    document.getElementById("pbox3").style.display = "block";
+
+    document.getElementById("pbox1").style.display = "none";
+
+    document.getElementById("pbox2").style.display = "none";
+}
